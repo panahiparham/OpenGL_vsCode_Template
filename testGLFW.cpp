cb42e2faad5dbfbb598cdf26aebcadda6a5de9ca
Parham-M-Panahi <panahiparham@gmail.com>
2020-12-02 16:22:29 +0330

    .

--- testGLFW.cpp
+++ testGLFW.cpp
@@ -1,4 +1,11 @@
+#define GLEW_STATIC
+
+
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
+
 
 int main(void)
 {
@@ -8,7 +15,7 @@ int main(void)
     if (!glfwInit())
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
+    /* Create a windowed mode window and its OpenGL cont"/LIBPATH:${workspaceFolder}\\dependencies\\GLFW\\lib",ext */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
@@ -19,11 +26,31 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        /* Problem: glewInit failed, something is seriously wrong. */
+        std::cout << glewGetErrorString(err) << std::endl;
+
+    }
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f( 0.0f,  0.5f);
+        glVertex2f( 0.5f, -0.5f);
+
+        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
